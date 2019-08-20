9a2926ee05bf28051c4ce64d8868f509b8bda5d3
Azat Orazov <orazovazat@gmail.com>
2019-08-20 00:24:38 -0700

    added test2

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -4,5 +4,6 @@ public class FirstTest {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Test");
+        System.out.println("Test2");
     }
 }
