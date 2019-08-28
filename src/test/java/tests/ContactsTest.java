9643658c8aa9f9ed6bf0ad9c09a72931c79f042f
Azat Orazov <orazovazat@gmail.com>
2019-08-27 21:17:16 -0700

    Modified Contacts tests

--- src/test/java/tests/ContactsTest.java
+++ src/test/java/tests/ContactsTest.java
@@ -1,4 +1,12 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class ContactsTest {
+
+    @Test
+    public void test1(){
+        System.out.println("driver open vytrack");
+        System.out.println("go to contacts page");
+    }
 }
